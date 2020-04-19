0d653d13cc860bb2ee14bd4769311ee96f254b9d
Wil Goolsby <wil@wilgoolsby.com>
2020-04-19 09:15:55 -0400

    capture clicks and put them in the results

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let selectedEntry = 0;
+
+const result = document.getElementById('result');
+
 const add = (a, b) => a + b;
 
 const subtract = (a, b) => a - b;
@@ -7,3 +11,13 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 const operate = (a, b, op) => {};
+
+// Grab a nodelist from all of the number buttons.
+const numbers = document.querySelectorAll('.number-btn');
+//operate(e.target.dataset.key)
+numbers.forEach((number) =>
+    number.addEventListener(
+        'click',
+        (e) => (result.textContent = parseInt(e.target.dataset.key))
+    )
+);
